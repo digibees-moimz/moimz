2c439d77989bf5de1e29e44e700f9ccb81beb92c
박단비 <silverwill39@gmail.com>
2025-04-28 01:39:17 +0900

    [FE] fix #6: tailwind-merge 적용하여 클래스 병합 오류 수정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function HomePage() {
   return (
-      <div className="text-center text-xl font-semibold">
-        🎉 moiMz에 오신 걸 환영합니다!
-      </div>
+    <div className="text-center text-xl font-semibold">
+      🎉 moiMz에 오신 걸 환영합니다!
+    </div>
   );
 }
--- frontend/src/components/ui-components/layout/Flex.tsx
+++ frontend/src/components/ui-components/layout/Flex.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import type { TagName } from "@/types";
+import { twMerge } from "tailwind-merge";
 
 interface FlexProps extends React.HTMLAttributes<HTMLElement> {
   as?: TagName;
 }
 
 function createHTMLElement(props: FlexProps, className: string) {
-  return React.createElement(props.as || "div", { className, ...props });
+  const { as, className: propsClassName, ...restProps } = props;
+  // tailwind-merge를 사용하여 기본 클래스와 props로 전달된 클래스를 병합
+  const mergedClassName = twMerge(className, propsClassName);
+
+  return React.createElement(as || "div", {
+    className: mergedClassName,
+    ...restProps,
+  });
 }
 
 function createHTMLElementWithCurry(className: string) {
@@ -14,7 +22,7 @@ function createHTMLElementWithCurry(className: string) {
 }
 
 const ColCenter = createHTMLElementWithCurry(
-  "flex flex-col justify-center items-center w-full h-full"
+  "flex flex-col justify-center items-center w-full"
 );
 
 const RowCenter = createHTMLElementWithCurry(
@@ -22,7 +30,7 @@ const RowCenter = createHTMLElementWithCurry(
 );
 
 const ColBetweenCenter = createHTMLElementWithCurry(
-  "flex flex-col justify-between items-center h-full"
+  "flex flex-col justify-between items-center"
 );
 
 const RowBetweenCenter = createHTMLElementWithCurry(
@@ -30,7 +38,7 @@ const RowBetweenCenter = createHTMLElementWithCurry(
 );
 
 const ColStartCenter = createHTMLElementWithCurry(
-  "flex flex-col justify-start items-center h-full"
+  "flex flex-col justify-start items-center"
 );
 
 const RowStartCenter = createHTMLElementWithCurry(
@@ -38,7 +46,7 @@ const RowStartCenter = createHTMLElementWithCurry(
 );
 
 const ColEndCenter = createHTMLElementWithCurry(
-  "flex flex-col justify-end items-center h-full"
+  "flex flex-col justify-end items-center"
 );
 
 const RowEndCenter = createHTMLElementWithCurry(
