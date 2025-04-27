5a127a5886a51d4726c28260d735f974991afd1e
박단비 <silverwill39@gmail.com>
2025-04-28 00:59:46 +0900

    [FE] feat #6: 방향별 유틸리티 컴포넌트 추가

--- frontend/src/components/ui-components/layout/Flex.tsx
+++ frontend/src/components/ui-components/layout/Flex.tsx
@@ -13,4 +13,45 @@ function createHTMLElementWithCurry(className: string) {
   return (props: FlexProps) => createHTMLElement(props, className);
 }
 
-export const Flex = {};
+const ColCenter = createHTMLElementWithCurry(
+  "flex flex-col justify-center items-center w-full h-full"
+);
+
+const RowCenter = createHTMLElementWithCurry(
+  "flex flex-row justify-center items-center w-full"
+);
+
+const ColBetweenCenter = createHTMLElementWithCurry(
+  "flex flex-col justify-between items-center h-full"
+);
+
+const RowBetweenCenter = createHTMLElementWithCurry(
+  "flex flex-row justify-between items-center"
+);
+
+const ColStartCenter = createHTMLElementWithCurry(
+  "flex flex-col justify-start items-center h-full"
+);
+
+const RowStartCenter = createHTMLElementWithCurry(
+  "flex flex-row justify-start items-center"
+);
+
+const ColEndCenter = createHTMLElementWithCurry(
+  "flex flex-col justify-end items-center h-full"
+);
+
+const RowEndCenter = createHTMLElementWithCurry(
+  "flex flex-row justify-end items-center"
+);
+
+export const Flex = {
+  ColCenter,
+  RowCenter,
+  ColBetweenCenter,
+  RowBetweenCenter,
+  ColStartCenter,
+  RowStartCenter,
+  ColEndCenter,
+  RowEndCenter,
+};
