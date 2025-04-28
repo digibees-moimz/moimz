e6222c1932d290042fc68952f318aee6339d8e79
박단비 <silverwill39@gmail.com>
2025-04-28 19:16:29 +0900

    [FE] feat #8: Grid 유틸리티 컴포넌트 추가

---
+++ frontend/src/components/ui-components/layout/Grid.tsx
@@ -0,0 +1,22 @@
+import { createHTMLElementWithCurry } from "./shared/createElementWithClassName";
+
+// 기본 Grid
+const BaseGrid = createHTMLElementWithCurry("grid gap-4");
+
+// 고정 열수
+const Col2 = createHTMLElementWithCurry("grid grid-cols-2 gap-4");
+const Col3 = createHTMLElementWithCurry("grid grid-cols-3 gap-4");
+const Col4 = createHTMLElementWithCurry("grid grid-cols-4 gap-4");
+
+// 반응형 AutoGrid
+const AutoCols = createHTMLElementWithCurry(
+  "grid gap-4 grid-cols-[repeat(auto-fit,minmax(140px,1fr))]"
+);
+
+export const Grid = {
+  Grid: BaseGrid,
+  Col2,
+  Col3,
+  Col4,
+  AutoCols,
+};
