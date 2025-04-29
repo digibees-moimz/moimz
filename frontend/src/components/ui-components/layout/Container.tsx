6c758efb3806ccf754e41a762c46fafc3096198a
박단비 <silverwill39@gmail.com>
2025-04-29 15:08:24 +0900

    [FE] feat #10: Container 시스템 구축

---
+++ frontend/src/components/ui-components/layout/Container.tsx
@@ -0,0 +1,5 @@
+import { createHTMLElementWithCurry } from "./shared/createElementWithClassName";
+
+export const Container = createHTMLElementWithCurry(
+  "w-full max-w-[1200px] mx-auto px-4 sm:px-6 md:px-8"
+);
