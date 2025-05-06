7550b183d0eecb42db904acd2ceafac8e4485e7b
박단비 <silverwill39@gmail.com>
2025-05-07 00:02:49 +0900

    [FE] feat #48: 출석 페이지 UI 구현

--- frontend/src/app/groups/[groupId]/attendance/page.tsx
+++ frontend/src/app/groups/[groupId]/attendance/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
 import { Typography } from "@/components/ui-components/typography/Typography";
+import { Flex } from "@/components/ui-components/layout/Flex";
+import { navigateWithScrollTop } from "@/utils/navigation";
 
 export default function AttendancePage() {
+  const router = useRouter();
   const { groupId } = useParams();
   const groupIdNumber = Number(groupId);
 
@@ -12,9 +16,125 @@ export default function AttendancePage() {
   }
 
   return (
-    <div className="p-4">
-      <Typography.Heading3>출석 체크</Typography.Heading3>
-      {/* 출석 관련 컴포넌트 추가 예정 */}
-    </div>
+    <>
+      <Typography.Heading3 className="font-bold">출석체크</Typography.Heading3>
+      <Flex.ColCenter className="gap-5">
+        {/* 출석 관련 컴포넌트 추가 예정 */}
+        <Image
+          src="/icons/together.png"
+          alt="단디 똑디 우디"
+          width={320}
+          height={100}
+        />
+        <Flex.ColCenter>
+          <Typography.BodySmall>
+            출석만 하면 똑똑하게 정산 끝!
+          </Typography.BodySmall>
+          <Typography.Caption>
+            사진 또는 수동으로 출석체크만 하면
+          </Typography.Caption>
+          <Typography.Caption>
+            출석한 멤버끼리 모임비가 자동으로 N등분 결제해드려요!
+          </Typography.Caption>
+        </Flex.ColCenter>
+
+        {/* 사진 기반 출석체크 */}
+        <button
+          className="bg-[#E5EFFE] w-full p-3 rounded-lg hover:bg-[#d1e1f9] mt-7 cursor-pointer"
+          onClick={() =>
+            navigateWithScrollTop(router, `/groups/${groupId}/attendance/photo`)
+          }
+        >
+          <Flex.RowStartCenter className="gap-6">
+            <Image
+              src="/icons/photo.png"
+              alt="사진 아이콘"
+              width={75}
+              height={75}
+            />
+            <Flex.ColStartStart>
+              <Typography.Heading5 className="pb-1">
+                사진 출석체크
+              </Typography.Heading5>
+              <>
+                <Typography.Caption as="p">
+                  얼굴을 인식해 자동으로 출석 완료!
+                </Typography.Caption>
+                <Typography.Caption as="p">
+                  출석도 하고, 소중한 순간도 함께 남겨요.
+                </Typography.Caption>
+              </>
+            </Flex.ColStartStart>
+          </Flex.RowStartCenter>
+        </button>
+
+        {/* 위치 기반 출석체크 */}
+        <button
+          className="bg-[#FFF3F6] w-full p-3 rounded-lg hover:bg-[#f6e2e0] cursor-pointer"
+          onClick={() =>
+            navigateWithScrollTop(
+              router,
+              `/groups/${groupId}/attendance/location`
+            )
+          }
+        >
+          <Flex.RowStartCenter className="gap-6">
+            <Image
+              src="/icons/location.png"
+              alt="지도 아이콘"
+              width={75}
+              height={75}
+            />
+            <Flex.ColStartStart>
+              <Typography.Heading5 className="pb-1">
+                위치 출석체크
+              </Typography.Heading5>
+              <>
+                <Typography.Caption as="p">
+                  한 명만 눌러도 근처 멤버 모두 출석 완료!
+                </Typography.Caption>
+                <Typography.Caption as="p">
+                  빠르게 출석을 끝내보세요.
+                </Typography.Caption>
+              </>
+            </Flex.ColStartStart>
+          </Flex.RowStartCenter>
+        </button>
+
+        {/* 수동 출석체크 */}
+        <button
+          className="bg-[#F4FBCF] w-full p-4 rounded-lg hover:bg-[#EEF6C2] cursor-pointer"
+          onClick={() =>
+            navigateWithScrollTop(
+              router,
+              `/groups/${groupId}/attendance/manual`
+            )
+          }
+        >
+          <Flex.RowStartCenter className="gap-6">
+            <Image
+              src="/icons/manual.png"
+              alt="체크 아이콘"
+              width={65}
+              height={65}
+            />
+            <Flex.ColStartStart>
+              <Typography.Heading5 className="pb-1">
+                수동 출석체크
+              </Typography.Heading5>
+              <>
+                <Typography.Caption>
+                  이름만 선택하면 바로 출석 완료!
+                </Typography.Caption>
+
+                <Typography.Caption as="p">
+                  사진 찍기 어려울 때 간편하게 사용하세요.
+                </Typography.Caption>
+              </>
+            </Flex.ColStartStart>
+          </Flex.RowStartCenter>
+        </button>
+      </Flex.ColCenter>
+    </>
   );
 }
--- frontend/src/components/ui-components/typography/Typography.tsx
+++ frontend/src/components/ui-components/typography/Typography.tsx
@@ -10,16 +10,21 @@ const Heading1 = createHTMLElementWithCurry(
   "h1"
 );
 const Heading2 = createHTMLElementWithCurry(
-  "text-2xl sm:text-3xl font-semibold leading-snug",
+  "text-2xl sm:text-3xl font-bold leading-snug",
   "h2"
 );
 const Heading3 = createHTMLElementWithCurry(
-  "text-xl sm:text-2xl font-semibold leading-snug",
+  "text-xl sm:text-2xl font-bold leading-snug",
   "h3"
 );
 
 const Heading4 = createHTMLElementWithCurry(
-  "text-lg sm:text-xl font-semibold leading-snug",
+  "text-lg sm:text-xl font-bold leading-snug",
+  "h3"
+);
+
+const Heading5 = createHTMLElementWithCurry(
+  "text-base sm:text-lg font-bold leading-snug",
   "h3"
 );
 
@@ -44,6 +49,7 @@ export const Typography = {
   Heading2,
   Heading3,
   Heading4,
+  Heading5,
   BodyLarge,
   Body,
   BodySmall,
