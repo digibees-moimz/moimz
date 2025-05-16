c3ba1bb04d2e6c0ed6c85bf6e9e3ca8c9b59d498
박단비 <silverwill39@gmail.com>
2025-05-16 23:38:42 +0900

    [FE] feat #74: 결제가능 금액에 후불결제 포함

--- frontend/src/components/attendance/AttendanceSummaryCard.tsx
+++ frontend/src/components/attendance/AttendanceSummaryCard.tsx
@@ -11,7 +11,7 @@ export const AttendanceSummaryCard = ({
   availableToSpend: number;
 }) => {
   const minAmount = Math.min(...attendees.map((p) => p.locked_amount));
-  const perPersonAmount = Math.floor(availableToSpend / count);
+  const totalAvailableToSpend = Math.floor(availableToSpend * count);
 
   return (
     <div className="p-5 mt-6 bg-[#EEFAF7] rounded-xl space-y-1 text-sm text-gray-800">
@@ -19,21 +19,41 @@ export const AttendanceSummaryCard = ({
         출석한 인원 - {count}명
       </Typography.Body>
 
-      <div className="flex justify-between p-1">
-        <span>출석자 중 최소 금액</span>
-        <span>{Math.round(minAmount).toLocaleString()} 원</span>
+      <div className="flex flex-col">
+        <span className=" pt-2 font-bold p-1">1인당 결제 가능 금액</span>
+        <div className="flex justify-between p-1 pl-2">
+          <span>락인 기준</span>
+          <span className="text-[#22BD9C] text-base">
+            {Math.round(minAmount).toLocaleString()} 원
+          </span>
+        </div>
+
+        <div className="flex justify-between p-1 pl-2">
+          <span>후불 결제 포함</span>
+          <span className="text-[#22BD9C] text-base">
+            {Math.round(minAmount + 100000).toLocaleString()} 원
+          </span>
+        </div>
       </div>
 
-      <div className="flex justify-between p-1">
-        <span>1인당 결제 가능 금액</span>
-        <span>{Math.round(perPersonAmount).toLocaleString()} 원</span>
-      </div>
+      <div className="flex flex-col border-t border-gray-200 mt-3">
+        <span className=" pt-2 font-bold p-1">총 결제 가능 금액</span>
+        <div className="flex justify-between p-1 pl-2">
+          <span>락인 기준</span>
+          <span className="text-[#22BD9C] text-base">
+            {Math.round(totalAvailableToSpend).toLocaleString()} 원
+          </span>
+        </div>
 
-      <div className="flex justify-between border-t mt-3 pt-2 border-gray-200 font-bold p-1">
-        <span>총 결제 가능 금액</span>
-        <span className="text-[#22BD9C] text-base">
-          {Math.round(availableToSpend).toLocaleString()} 원
-        </span>
+        <div className="flex justify-between p-1 pl-2">
+          <span>후불 결제 포함</span>
+          <span className="text-[#22BD9C] text-base">
+            {Math.round(
+              totalAvailableToSpend + 100000 * count
+            ).toLocaleString()}{" "}
+            원
+          </span>
+        </div>
       </div>
     </div>
   );
